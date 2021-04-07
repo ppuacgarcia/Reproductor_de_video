cfc2c77e9c0a5af0ccc222fce167d73b43c37a02
Carlos Cancinos <61300585+carloscancinos12@users.noreply.github.com>
2021-04-07 01:34:50 -0600

    solo para guardar mi ruta, perdonar

--- leertxt/src/main/java/com/mycompany/leertxt/NewMain.java
+++ leertxt/src/main/java/com/mycompany/leertxt/NewMain.java
@@ -7,6 +7,7 @@ public class NewMain {
     public static void main(String[] args) {
         Leer variable=new Leer();
         String rut="\\leertxt\\src\\main\\java\\com\\mycompany\\leertxt\\rutas.txt";
+        //"C:\\Users\\Silver VG\\Desktop\\De carlitos\\Reproductor_de_video\\leertxt\\src\\main\\java\\com\\mycompany\\leertxt\\rutas.txt"
         variable.leertxt(rut);
     }
     
